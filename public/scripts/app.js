7fd32cf2488ea72e8d3bbb4b6d2576ec02cfa7c1
Rakesh Patel <emailme@rakeshrpatel.com>
2016-09-24 17:51:53 -0400

    new updates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,73 +5,83 @@
 // Create client-side to-do list and lets users add/remove
 
 angular.module('momentumArtApp', [])
-    .controller('backgroundImage', ['$scope', '$http', function($scope, $http) {
-        var d = new Date();
-        console.log(new Date());
-        $scope.time = d.getHours() + ':' + d.getMinutes();
-        $scope.date = d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear();
-        $scope.response = getBackgroundImage();
+    .controller('backgroundImage', ['$scope', '$http', '$log', function($scope, $http, $log) {
+        $scope.time = updatedClock();
+        // $scope.getBackgroundImage = getBackgroundImage();
 
-        var getBackgroundImage = function() {
-            var params = {
-                'key': '3367371-c4da2ff0f6b2449e818db68b5',
-                'q': 'yellow flowers',
-                'image_type': 'photo'
-            }
+        // THIS IS WORKING BUT GETTING CROSS_ORIGIN PROB w/ getImage FUNCTION
+        // function getBackgroundImage() {
+        //     // var params = {
+        //     //     'key': '3367371-c4da2ff0f6b2449e818db68b5',
+        //     //     'q': 'yellow flowers',
+        //     //     'image_type': 'photo',
+        //     //     // special permission - applied
+        //     //     // 'response_group': 'high_resolution'
+        //     // }
+        //     var params = {
+        //         'client_id': 'cd7051715d376f899232',
+        //         'client_secret': 'de9378d3d12c2cbfb24221e8b96d212c',
+        //     }
 
-            $http({
-                    url: 'https://pixabay.com/api',
-                    method: 'GET',
-                    params: params
-                })
-                .then(function(response) {
-                    console.log(response);
-                }, function(error, image) {
-                    if (error) {
-                        console.log(error)
-                    }
-                    console.log(image);
-                    return image;
-                })
-
-        }
-
-        // getBackgroundImage();
-        // getQuote();
-
-        // var getBackgroundImage = function() {
-        //             var params = {
-        //                 'API': '3367371-c4da2ff0f6b2449e818db68b5',
-        //                 'q': 'yellow flowers',
-        //                 'image_type': 'photo'
-        //             }
-        //             $http({
-        //                 method: 'GET',
-        //                 url: 'https://pixabay.com/api',
-        //                 params: params
+        //     $http({
+        //             method: 'POST',
+        //             url: 'https://api.artsy.net/api/tokens/xapp_token',
+        //             params: params
+        //         })
+        //         .then(function(response) {
+        //                 // $scope.getBackgroundImage = response.data.hits[0].webformatURL + ;
+        //                 var authorization = response.data.token
+        //                 console.log('authorization1-------', authorization);
+        //                 getImage(authorization);
+        //                 $log.info(response);
+        //             },
+        //             function(error) {
+        //                 $log.info(error);
         //             })
 
-        //             .then(function(response) {
-        //                     $scope.response = response;
-        //                 }),
-        //                 // Handles errors?
-        //                 function(data, status, headers, config) {
-        //                     console.log('Failure');
-        //                     // called when an error occurs or
-        //                     // the server returns data with an error status
-        //                 };
-        //         };
-
-        // var getQuote = function() {
-        //     $http({
-        //         method: 'GET',
-        //         url: 'https://favqs.com/api/qotd'
-        //     })
-
-        //     .then(function(response) {
-        //         console.log(response)
-        //     })
         // }
+
+        // var getImage = function(authorization) {
+        //     console.log('authorization in getImage-------', authorization);
+        //     var params = {
+        //     	'Accept': 'application/vnd.artsy-v2+json',
+        //     	'X-Xapp-Token': authorization
+        //     }
+        //     $http({
+        //             method: 'GET',
+        //             url: 'https://api.artsy.net/api/artists/andy-warhol',
+        //             headers: params
+        //         })
+        //         .then(function(response) {
+        //                 // $scope.getBackgroundImage = response.data.hits[0].webformatURL + ;
+        //                 $log.info(response);
+        //             },
+        //             function(error) {
+        //                 $log.info(error);
+        //             })
+
+        // }
+
+
     }]);
 
-//https://pixabay.com/api/?key=3367371-c4da2ff0f6b2449e818db68b5&q=yellow+flowers&image_type=photo
+var updatedClock = function() {
+    // setInterval(updatedClock, 1000);
+    var currentTime = new Date();
+    // console.log(new Date());
+    return currentTime.getHours() + ':' + currentTime.getMinutes();
+}
+
+// var getQuote = function() {
+//     $http({
+//         method: 'GET',
+//         url: 'https://favqs.com/api/qotd'
+//     })
+
+//     .then(function(response) {
+//         console.log(response)
+//     })
+// }
+
+
+// https://pixabay.com/api/?key=3367371-c4da2ff0f6b2449e818db68b5&q=yellow+flowers&image_type=photo
--- server.js
+++ server.js
@@ -1,52 +1,49 @@
+// Tried browserifying main.js and sever.js - no success; same error of require not defined
+
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     http = require('http');
-    // request = require('superagent');
+    path = require('path');
+// request = require('superagent');
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/bundle.js', express.static(__dirname + '/bundle.js'))
+app.use('/server.js', express.static(__dirname + '/server.js'));
+
+// app.use('/public', express.static(__dirname + '/public'));
+// Tries:
+// app.use('/public', express.static(__dirname + '/public'));
+// Rearranging order of when use files are processed
+// /public is tempermental
+
+app.get('/', function(req, res) {
+    // Tried directly loading it
+    console.log("Hello world");
+});
 
 app.get('/example', function(req, res) {
     res.status(201).json({});
 })
 
-// http.get({
-//     host: 'https://pixabay.com/api/',
-//     path: 
-// })
-
-// var url = 'https://api.shutterstock.com/v2/images/recommendations';
-// request
-//     .get(url).end(function(res, err){
-//         if (err){
-//             console.log(err)
-//         }
-//         console.log(res);
-//         res.json(res);
-//     })
-
-// For artsy
-// var getAppToken = function(req, res) {
-//     var clientID = 'cd7051715d376f899232',
-//         clientSecret = 'de9378d3d12c2cbfb24221e8b96d212c',
-//         apiUrl = 'https://api.artsy.net/api/tokens/xapp_token',
-//         xappToken;
-
-//     request
-//         .post(apiUrl)
-//         .send({ client_id: clientID, client_secret: clientSecret })
-//         .end(function(res, err) {
-//             if (req.body.token) {
-//                 xappToken = res.body.token;
-//             } else {
-//                 console.log(err)
-//             };
-
-//         });
-// }
-
-
+app.get('/authorization', function(req, res) {
+    var responseHere;
+    var params = {
+        'client_id': 'cd7051715d376f899232',
+        'client_secret': 'de9378d3d12c2cbfb24221e8b96d212c',
+    }
+    http.request({
+        host: 'https://api.artsy.net/api/tokens/xapp_token',
+        method: 'POST',
+        headers: {params}
+    }, function(response) {
+        console.log('this is the response--------', response)
+        responseHere = response
+    })
+    res.send(responseHere)
+})
 
 app.listen(process.env.PORT || 8080);
 console.log('Connected Captain. Safe journey.');
